d37fe943d302595c6f1cd6ccba5b475ef065ae3a
AlejandroRondon <rondon_udea@hotmail.com>
2017-05-09 19:16:44 -0500

    Advance 2

--- class2.rb
+++ class2.rb
@@ -1,1 +1,29 @@
+=begin
 puts 1,'Hola'
+puts 'Ingresa por favor tu nombre' 	#show in console
+nombre = gets						#receive from console
+puts 'Nombre con caracteres especiales', nombre
+p nombre # 						show content variable
+puts 'Ingresa por favor tu nombre2' 	#show in console
+nombre2 = gets.chomp
+puts 'Nombre 2 Sin caracteres especiales', nombre2
+p nombre2 #show content variable
+=end
+
+puts 'Please, enter your name: '
+myName = gets.chomp
+puts 'Please, Enter your age: '
+age = gets.chomp
+puts 'Show in console mode 1:'
+puts 'your name is', myName, ', your age is: ',age
+puts 'Show in console mode 2:'
+puts "Your name is #{myName}, your age is #{age}"
+
+puts 'Please, enter number 1: '
+number1 = gets
+puts 'Please, enter number 2: '
+number2 = gets
+puts 'Please, enter number 3: '
+number3 = gets
+result = number1+number2+number3
+puts "Numbers: #{number1},#{number2},#{number3}. average is #{result}"
